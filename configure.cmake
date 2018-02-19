db251159b3bf18c87059ec33e7fef2dc61477ac2
Hiroshi Miura <miurahr@linux.com>
2018-02-19 09:24:59 +0900

    [cmake] Update dependencies
    - Optional selection of CharLS, LZMA, OpenJPEG2 and WEBP.
    - Detect OpenJPEG versions and include path.

--- configure.cmake
+++ configure.cmake
@@ -16,9 +16,15 @@ check_function_exists(sqrt HAVE_SQRT)
 
 check_include_file("dlfch.h" HAVE_DLFCH_H)
 check_include_file("float.h" HAVE_FLOAT_H)
+
 check_include_file("geotiff/geotiff.h" HAVE_GEOTIFF_GEOTIFF_H)
 check_include_file("geotiff.h" HAVE_GEOTIFF_H)
 check_include_file("gif_lib.h" HAVE_GIF_LIB_H)
+check_include_file("openjpeg-2.0/openjpeg.h" HAVE_OPENJPEG_2_0_OPENJPEG_H)
+check_include_file("openjpeg-2.1/openjpeg.h" HAVE_OPENJPEG_2_1_OPENJPEG_H)
+check_include_file("openjpeg-2.2/openjpeg.h" HAVE_OPENJPEG_2_2_OPENJPEG_H)
+check_include_file("openjpeg-2.3/openjpeg.h" HAVE_OPENJPEG_2_3_OPENJPEG_H)
+
 check_include_file("inttypes.h" HAVE_INTTYPES_H)
 check_include_file("jerror.h" HAVE_JERROR_H)
 check_include_file("jpeglib.h" HAVE_JPEGLIB_H)
